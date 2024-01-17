3a5ad88b61e24adfd7fe943d08d063c43d399300
Anduamlak yohannis <155321477+Anduamlk@users.noreply.github.com>
2024-01-17 10:57:31 -0700

    Add files via upload

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,12 @@
-print(_:_:_)
+gtst ="Hello Today is our Day7......."
+print(gtst)
+
+mylist =[0,2,4]
+print("this are even number",mylist)
+
+
+dic ={"apple":10,'banana':15,'pinapple':20}
+print(dic)
 
         
 
